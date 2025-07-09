2b2a9a99f7c0d74f856e21cbb4bf693ac0e4d2a4
vagnercardosomagalhaes <contato.vagner.cardoso@gmail.com>
2025-07-09 16:04:08 -0300

    Tentativa de publicação 1

--- consultoriodigital/settings.py
+++ consultoriodigital/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4%_k=9o!mb9@cq@8i3+jbrfx5@h#vgj1y21isb=r09bdu2zog0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*'] # *******************************************************
 
